1cabcff5b5348ec92dabe82fe9a26a3db8285574
igoryan <nkulinigor1984@icloud.com>
2022-04-24 19:40:04 +0200

    task_001

---
+++ task_001/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача №1, примеры все прорешал, приложил решение последнего:
+int a = -9;
+int b = -3;
+int max = a;
+if (a > max) max = a; 
+if (b > max) max = b;
+Console.WriteLine("max =");
+Console.Write(max);
