03f0e5132467a01e81bfc11ff89fbd817d129c4d
igoryan <nkulinigor1984@icloud.com>
2022-04-24 19:43:06 +0200

    task_002

---
+++ task_002/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача №2, примеры все прорешал, приложил решение последнего:
+int a = 2;
+int b = 3;
+int c = 9;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.WriteLine("max =");
+Console.Write(max);
