e8cdb859fdd34e700ef4ac96937bcce6c0ba8a5e
igoryan <nkulinigor1984@icloud.com>
2022-04-24 20:14:05 +0200

    task_003

--- task_003/Program.cs
+++ task_003/Program.cs
@@ -3,9 +3,9 @@ int a = 7;
 int b = 2;
 if (a - (a / b) * b == 0)
 {
-    Console.WriteLine("Yes");
+    Console.WriteLine("Чётное");
 }
 else
 {
-    Console.WriteLine("No");
+    Console.WriteLine("Нечётное");
 }
